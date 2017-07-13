42fea7296f80ecfb397a6613d10f4aff9565195c
NaiaraBouza <naiarabouza@gmail.com>
2017-07-13 13:53:54 +0200

    add distance sensor

--- motor.ino
+++ motor.ino
@@ -1,3 +1,5 @@
+const int EchoPin = 12;
+const int TriggerPin = 11;
 int ENB = 8;
 int IN1 = 7;
 int IN2 = 6;
@@ -5,29 +7,10 @@ int IN3 = 5;
 int IN4 = 4;
 int ENA = 3;
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode (ENB, OUTPUT);
-  pinMode (ENA, OUTPUT);
-  pinMode (IN1, OUTPUT);
-  pinMode (IN2, OUTPUT);
-  pinMode (IN3, OUTPUT);
-  pinMode (IN4, OUTPUT);
-
-}
-
- void loop() {
-  // put your main code here, to run repeatedly:
-  analogWrite (ENB, 255);
-  analogWrite (ENA, 255);
-  /*digitalWrite (IN1, HIGH);
-  digitalWrite (IN2, LOW);
-  digitalWrite (IN3, HIGH);
-  digitalWrite (IN4, LOW);*/
-  Parar();
-}
 
 void Adelante (){
+  analogWrite (ENA, 255);
+  analogWrite (ENB, 255);
   digitalWrite (IN1, HIGH);
   digitalWrite (IN2, LOW);
   digitalWrite (IN3, HIGH);
@@ -35,6 +18,8 @@ void Adelante (){
 }
 
 void Atras () {
+   analogWrite (ENA, 255);
+  analogWrite (ENB, 255);
   digitalWrite (IN1, LOW);
   digitalWrite (IN2, HIGH);
   digitalWrite (IN3, LOW);
@@ -42,12 +27,16 @@ void Atras () {
 }
 
 void Izquierda () {
+   analogWrite (ENA, 255);
+  analogWrite (ENB, 255);
   digitalWrite (IN1, LOW);
   digitalWrite (IN2, HIGH);
   digitalWrite (IN3, HIGH);
   digitalWrite (IN4, LOW);
 }
 void Derecha () {
+   analogWrite (ENA, 255);
+  analogWrite (ENB, 255);
   digitalWrite (IN1, HIGH);
   digitalWrite (IN2, LOW);
   digitalWrite (IN3, LOW);
@@ -57,6 +46,47 @@ void Derecha () {
 void Parar () {
   analogWrite (ENA,0);
   analogWrite (ENB,0);
+}
+
+int ping(int TriggerPin, int EchoPin) {
+   long duration, distanceCm;
+
+   digitalWrite(TriggerPin, LOW);  //para generar un pulso limpio ponemos a LOW 4us
+   delayMicroseconds(4);
+   digitalWrite(TriggerPin, HIGH);  //generamos Trigger (disparo) de 10us
+   delayMicroseconds(10);
+   digitalWrite(TriggerPin, LOW);
+
+   duration = pulseIn(EchoPin, HIGH);  //medimos el tiempo entre pulsos, en microsegundos
+
+   distanceCm = duration * 10 / 292/ 2;   //convertimos a distancia, en cm
+   return distanceCm;
+}
+
+
+void setup()
+{
+  // put your setup code here, to run once:
+  pinMode (ENB, OUTPUT);
+  pinMode (ENA, OUTPUT);
+  pinMode (IN1, OUTPUT);
+  pinMode (IN2, OUTPUT);
+  pinMode (IN3, OUTPUT);
+  pinMode (IN4, OUTPUT);
+  pinMode(TriggerPin, OUTPUT);
+  pinMode(EchoPin, INPUT);
+  Serial.begin(9600);
+}
+
+
+ void loop() {
+  int cm = ping(TriggerPin, EchoPin);
+  Serial.print("Distancia: ");
+  Serial.println(cm);
+  delay(1000);
+  /*analogWrite(ENA,255);
+  analogWrite(ENB, 255);*/
+
 }
 
  
